29e1ba4ffc5c1d3b2e7660dd7b8262545b3bd38d
kys-code <dendenbo123@gmail.com>
2026-06-10 06:29:32 +0900

    Publish Memo Magic PWA

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "memo-magic-mobile-v2";
+const APP_SHELL = [
+  "./memo-magic-mobile.html",
+  "./manifest.webmanifest",
+  "./pwa-icon-192.png",
+  "./pwa-icon-512.png",
+  "./pwa-icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys
+        .filter((key) => key !== CACHE_NAME)
+        .map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put("./memo-magic-mobile.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./memo-magic-mobile.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request))
+  );
+});
